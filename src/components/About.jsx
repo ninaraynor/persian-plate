9d2718a8f0c18c965f778e9f8d7c33aaef8b9f9b
ninaraynor <ninaraynor@gmail.com>
2024-02-15 10:04:31 -0500

    Add text to about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,10 @@
 
 const About = () => {
     return (
-        <div>About</div>
+        <div className="about-container">
+            <h1>About Persian Plate</h1>
+            <p>Persian Plate is dedicated to preserving and sharing the rich culinary traditions of Persian cuisine. Through our platform, users can explore a diverse collection of authentic recipes passed down through generations, each with its own unique story and cultural significance.</p>
+        </div>
     )
 }
 
