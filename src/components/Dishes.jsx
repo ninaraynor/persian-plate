9c215aec07e55fa5ca38492ef08faad43f87f193
ninaraynor <ninaraynor@gmail.com>
2024-02-15 15:07:23 -0500

    debug dishes page

--- src/components/Dishes.jsx
+++ src/components/Dishes.jsx
@@ -7,8 +7,8 @@ const Dishes = ({ dishes }) => {
             <div className="dishes-grid">
             {dishes.map(dish => (
                 <div key={dish._id} className="dish-card">
-                <h2>{dish.title}</h2>
-                <h3>{dish.description}</h3>
+                <h3>{dish.title}</h3>
+                <p>{dish.description}</p>
                 <img src={dish.image} alt={dish.title}/>
                 <Link to={`/dishes/${dish._id}/recipes`}>View Recipes</Link>
                 </div>
