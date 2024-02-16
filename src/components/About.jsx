0c7549cd44bb68ed0428bdd8eabc71eb139adbd1
ninaraynor <ninaraynor@gmail.com>
2024-02-16 12:46:35 -0500

    style changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div className="about-container">
             <h2>About Persian Plate</h2>
-            <p>Persian Plate is dedicated to preserving and sharing the rich culinary traditions of Persian cuisine. Through our platform, users can explore a diverse collection of authentic recipes passed down through generations, each with its own unique story and cultural significance.</p>
+            <p className="aboout-text">Persian Plate is dedicated to preserving and sharing the rich culinary traditions of Persian cuisine. Through our platform, users can explore a diverse collection of authentic recipes passed down through generations, each with its own unique story and cultural significance.</p>
         </div>
     )
 }
